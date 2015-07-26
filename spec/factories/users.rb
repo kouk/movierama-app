3bb4cbd09ec23e9c5bedf584cef259501b39d4e5
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-07-26 21:10:38 +0300

    add factory and spec for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email, 100) { |n| "person#{n}@example.com" }
+    password "password"
+    password_confirmation "password"
+    created_at Time.now
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+
+  it "#email returns a string" do
+    expect(@user.email).to match 'person[0-9]+@example.com'
+  end
 end
