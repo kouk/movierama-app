72b5a53fbe23491cd4f9ae4d763c3938a506c38f
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-07-27 08:46:51 +0300

    add some more user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,19 @@ require 'spec_helper'
 
 RSpec.describe User, type: :model do
 
-  before(:each) { @user = FactoryGirl.build(:user) }
+  context "creation" do
+    before(:each) { @user = FactoryGirl.build(:user) }
 
-  subject { @user }
+    subject { @user }
 
-  it { should respond_to(:email) }
+    it { should respond_to(:email) }
 
-  it "#email returns a string" do
-    expect(@user.email).to match 'person[0-9]+@example.com'
+    it "#email returns a string" do
+      expect(@user.email).to match 'person[0-9]+@example.com'
+    end
+  end
+  context "validations" do
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
   end
 end
