242caedc21f77aa00967412b5a37fc052453a6bb
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-07-29 12:56:40 +0300

    move login to a popup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions' }
+  as :user do
+      get '/login' => 'devise/sessions#new'
+      delete '/logout' => 'devise/sessions#destroy'
+  end
   root to: 'movies#index'
 end
