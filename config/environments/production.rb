ad9f40918bdc8164361e292f89a584ad9544ec3d
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-07-29 16:35:23 +0300

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.debug = true
+  config.assets.raise_runtime_errors = true
 end
