15fa7d5e96e4725580155ce858dccaf91c0b3bbd
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2015-07-29 22:39:30 +0300

    actually submit movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,18 @@
 class MoviesController < ApplicationController
   def index
   end
+  def create
+      @entry = Movie.create!(movie_params)
+      if @entry.valid? and @entry.save
+        flash[:notice] = "Movie successfully submitted"
+      else
+        flash.now[:error] = "Could not submit movie!"
+      end
+      redirect_to action: 'index'
+  end
+
+  private
+    def movie_params
+       params.require(:movie).permit(:title, :year)
+    end
 end
